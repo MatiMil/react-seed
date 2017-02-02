ae33557ec1bf3f251febaa5a50c6118d0298174c
Matijas Milos <matijas.milos@gmx.de>
2017-02-02 09:23:24 +0100

    Implement routing with index and initial page

--- src/js/client.js
+++ src/js/client.js
@@ -1,12 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Router, Route, IndexRoute, hashHistory } from "react-router"
 import { Provider } from "react-redux"
 
-import Layout from "./components/Layout"
+import Index from "./pages/Index"
+import InitialPage from "./pages/InitialPage"
+import Layout from "./pages/Layout"
 import store from "./store"
 
-const app = document.getElementById('app')
+const app = document.getElementById('app');
 
-ReactDOM.render(<Provider store={store}>
-  <Layout />
-</Provider>, app);
+ReactDOM.render(
+<Provider store={store}>
+	<Router history={hashHistory}>
+		<Route path="/" component={Layout}>
+			<IndexRoute component={Index}></IndexRoute>
+			<Route path="initial" name="initial" component={InitialPage}></Route>
+		</Route>
+	</Router>
+</Provider>, 
+app);
---
+++ src/js/pages/Index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class IndexPage extends React.Component {
+  render() {
+
+    return (
+      <div>
+        <h2>Index Page</h2>
+      </div>
+    );
+  }
+}
